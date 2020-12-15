5502e95146b7b02fea86ae30d94a494c8390e19f
Zak Albertson <zakalbertson10@gmail.com>
2020-12-15 15:41:25 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+let randomNumber = Math.floor(Math.random()* 10) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHigh = document.querySelector('.lowOrHigh');
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+    alert('I am a placeholder');
+}
+
+function checkGuess() {
+    let userGuess = Number(guessField.value)
+    if (guessCount === 1) {
+        guessField.textContent = 'Previous guesses: ';
+    }
+    guessField.textContent += userGuess + ' ';
+    if (userGuess === randomNumber) {
+        lastResult.textContent = 'Congratulations! You got it right!';
+        lastResult.getElementsByClassName.backgroundColor = 'green';
+        lowOrHigh.textContent = '';
+        setGameOver();
+    } else if (guessCount === 3) {
+        lastResult.textContent = '!!!Game Over!!!';
+        setGameOver();
+    } else {
+        lastResult.textContent = 'Wrong!';
+        lastResult.getElementsByClassName.backgroundColor = 'red';
+        if(userGuess < randomNumber) {
+            lowOrHigh.textContent = 'Last guess was too low!';
+        } else if(userGuess > randomNumber) {
+            lowOrHigh.textContent = 'Last guess was too high!';
+        }
+    }
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
+}
+
+guessSubmit.addEventListener('click', checkGuess);
+function setGameOver(){
+    guessField.disabled = true
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start a new Game';
+    document.body.append(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
+function resetGame() {
+    guessCount = 1;
+    const resetParas = document.querySelectorAll('.resultParas p');
+    for (let i = 0; i < resetParas.length; i++){
+        resetParas[i].textContent = '';
+    }
+    resetButton.parentNode.removeChild(resetButton);
+    
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+    
+    lastResult.getElementsByClassName.backgroundColor = 'white';
+
+    randomNumber = Math.floor(Math.random() * 10) +1;
+}
